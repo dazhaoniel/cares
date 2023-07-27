3f4a57ede2747c3db858694c4d16fb05db980686
Daniel Zhao <dazhaoniel@users.noreply.github.com>
2023-07-27 04:54:22 +0000

    wip refactor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import re
-
-# import sys
 import requests
 from flask import Flask
 from urllib.parse import urljoin
@@ -8,13 +6,13 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-rlc_regex = "rescue-good-food-becoming-waste"
+RLC = "rescue-good-food-becoming-waste"
 
-coalition_regex = "staff-mobile-soup-kitchen"
+COALITION = "staff-mobile-soup-kitchen"
 
-base_url = "https://www.newyorkcares.org"
+BASE_URL = "https://www.newyorkcares.org"
 
-login_url = "/user"
+LOGIN_URL = "/user"
 
 # Create the payload
 payload = {
@@ -25,41 +23,41 @@ payload = {
 }
 
 
-def sign_up(is_coalition=False, is_rlc=False):
+def login():
+    """Login New York Cares and return the current session"""
     with requests.Session() as s:
         # Get the page first for https
-        s.get(urljoin(base_url, login_url))
+        s.get(urljoin(BASE_URL, LOGIN_URL))
 
         # Post the payload to the site to log in
-        s.post(urljoin(base_url, login_url), data=payload)
+        s.post(urljoin(BASE_URL, LOGIN_URL), data=payload)
+    return s
 
-        # Navigate to the project listings page
-        l = s.get("https://www.newyorkcares.org/search/projects/results?location=496")
 
-    soup = BeautifulSoup(l.text, "html.parser")
+def signup_project(session, project):
+    """Sign up the project with the correct regex matching"""
+    listing = session.get(
+        "https://www.newyorkcares.org/search/projects/results?location=496"
+    )
+
+    soup = BeautifulSoup(listing.text, "html.parser")
 
     # Go to the last page
     last_page_url = soup.find("a", class_="last")["href"]
 
-    last_page = s.get(urljoin(base_url, last_page_url))
+    last_page = session.get(urljoin(BASE_URL, last_page_url))
 
     soup = BeautifulSoup(last_page.content, "html.parser")
 
-    # Find Coalition
-    if is_coalition:
-        project_link = soup.find("a", href=re.compile(r".*" + coalition_regex + "*"))
+    project_links = soup.findAll("a", href=re.compile(r".*" + project + "*"))
 
-    # Find RLC
-    elif is_rlc:
-        project_link = soup.findAll("a", href=re.compile(r".*" + rlc_regex + "*"))
-
-        # Find the Usq project
-        for link in project_link:
-            if link.find_parent("div", class_="project").find(
-                string=re.compile(".*Union Square.*")
-            ):
-                project_link = link
-                break
+    # Find the correct project
+    for link in project_links:
+        if link.find_parent("div", class_="project").find(
+            string=re.compile(".*Union Square.*")
+        ):
+            project_link = link
+            break
 
     while not project_link and soup.find("a", class_="prev"):
         prev = soup.find("a", class_="prev")["href"]
@@ -104,37 +102,31 @@ def sign_up(is_coalition=False, is_rlc=False):
 
 @app.route("/projects/coalition", methods=["GET"])
 def get_coalition():
-    sign_up(True, False)
+    """Sign up Coalition for the homeless"""
+    s = login()
+    signup_project(s, COALITION)
     return ("Signed up Coalition", 200)
 
 
 @app.route("/projects/rlc", methods=["GET"])
 def get_rlc():
-    sign_up(False, True)
+    """Sign up Rescue Leftover Cuisine"""
+    s = login()
+    signup_project(s, RLC)
     return ("Signed up RLC", 200)
 
 
 @app.route("/", methods=["GET"])
 def hello_world():
+    """Test route for the API"""
     return ("Hello World!", 200)
 
 
-# def main():
-#     is_coalition = False
-#     is_rlc = True
-
-#     if len(sys.argv) != 2:
-#         sys.exit("Add project name to sign up for")
-#     elif bool(re.match("coalition", sys.argv[1])):
-#         is_coalition = True
-#     elif bool(re.match("rlc", sys.argv[1])):
-#         is_rlc = True
-#     else:
-#         sys.exit()
-
-#     sign_up(is_coalition, is_rlc)
+def main():
+    s = login()
+    signup_project(s, COALITION)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # main()
+    # app.run(debug=True)
+    main()
